f8e5df0fdd49554daf56d4328920cfa36a1531ad
luwat <oluwatobioreniyi@gmail.com>
2022-03-03 10:18:30 +0100

    fixed, thanks @Ebube for the contribution

--- script/script.js
+++ script/script.js
@@ -10,26 +10,25 @@ let randomPick = Math.floor(Math.random() * pick.length);
 return pick[randomPick]; 
 }
 
-console.log(computerPlay(rock, paper, scissors));
 //create a variabe asking for human's input
 let humanInput = prompt(`${rock}? ${paper}? or ${scissors}?. Pls decide`)
 //create a function for human's choice or selection
 function humanPlay(rock, paper, scissors) {
-    if (humanInput === "Rock" || humanInput === "rock" || humanInput === "ROCK") {
+    if (humanInput.toLowerCase() === "rock") {
         console.log(rock);
         return rock;
-    } else if (humanInput === "Paper" || humanInput === "paper" || humanInput === "PAPER") {
+    } else if (humanInput.toLowerCase() === "paper") {
         console.log(paper);
         return paper;
-    } else if (humanInput === "Scissors" || humanInput === "scissors" || humanInput === "SCISSORS") {
+    } else if (humanInput.toLowerCase() === "scissors") {
         console.log(scissors);
         return scissors;
     } else {
         console.log("nothing to show");
+        return "nothing to show";
     }
 }
 
-//humanPlay(rock, paper, scissors);
 //create a function to compare human & computer selection
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -45,11 +44,13 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === paper && computerSelection === scissors) {
         console.log(`You lose! ${scissors} beats ${paper}.`);
     } else if (playerSelection === scissors && computerSelection === paper) {
-        console.log(`You lose! ${scissors} beats ${paper}.`);
+        console.log(`You win! ${scissors} beats ${paper}.`);
     }
 }
 const playerSelection = humanPlay(rock, paper, scissors);
 const computerSelection = computerPlay(rock, paper, scissors);
+console.log(computerSelection);
+console.log(playerSelection);
 
 playRound(playerSelection, computerSelection);
 //compare choice or selection and decide the winner
