bb69eeb806937273e17793a2bbcb2cdddbb28d92
luwat <oluwatobioreniyi@gmail.com>
2022-03-03 08:28:33 +0100

    inaccurate comparison, how do i solve this?

--- script/script.js
+++ script/script.js
@@ -11,10 +11,8 @@ return pick[randomPick];
 }
 
 console.log(computerPlay(rock, paper, scissors));
-
 //create a variabe asking for human's input
 let humanInput = prompt(`${rock}? ${paper}? or ${scissors}?. Pls decide`)
-
 //create a function for human's choice or selection
 function humanPlay(rock, paper, scissors) {
     if (humanInput === "Rock" || humanInput === "rock" || humanInput === "ROCK") {
@@ -31,11 +29,23 @@ function humanPlay(rock, paper, scissors) {
     }
 }
 
+//humanPlay(rock, paper, scissors);
 //create a function to compare human & computer selection
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log("it is a tie!");
-        return "it is a tie";
+    if (playerSelection === computerSelection) {
+        console.log("It is a tie");
+    } else if (playerSelection === rock && computerSelection === paper) {
+        console.log(`You lose! ${paper} beats ${rock}.`);
+    } else if (playerSelection === paper && computerSelection === rock) {
+        console.log(`You win! ${paper} beats ${rock}.`);
+    } else if (playerSelection === rock && computerSelection === scissors) {
+        console.log(`You win! ${rock} beats ${scissors}.`);
+    } else if (playerSelection === scissors && computerSelection === rock) {
+        console.log(`You lose! ${rock} beats ${scissors}.`);
+    } else if (playerSelection === paper && computerSelection === scissors) {
+        console.log(`You lose! ${scissors} beats ${paper}.`);
+    } else if (playerSelection === scissors && computerSelection === paper) {
+        console.log(`You lose! ${scissors} beats ${paper}.`);
     }
 }
 const playerSelection = humanPlay(rock, paper, scissors);
